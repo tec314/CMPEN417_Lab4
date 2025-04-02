710e55dc3eb9bd7ff53ce43a8899572c1ecc71d1
CollinRoss21 <cdr5451@psu.edu>
2025-04-02 17:29:15 -0400

    final with pragma

--- fpga417_lab4.c
+++ fpga417_lab4.c
@@ -3,6 +3,9 @@
 
 
 void fpga417_fir(int *data, int *filter) {
+	#pragma HLS INTERFACE mode=m_axi port=data offset=slave bundle=gmem0
+    #pragma HLS INTERFACE mode=m_axi port=filter offset=slave bundle=gmem1
+    #pragma HLS INTERFACE mode=s_axilite port=return bundle=BUS_A
 	int temp[LENGTH+2]={0};
 	int result;
 	for (int h = 0; h < LENGTH; h++){
@@ -10,6 +13,7 @@ void fpga417_fir(int *data, int *filter) {
 	}
 
 	for (int i = 2; i < LENGTH+2; i++) {
+
 	        fir(temp, data, i, filter);
 	    }
 
